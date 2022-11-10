24f4b834469defb9fc4623d6e9ee06b11c4825b9
Shpackk <victorija45@gmail.com>
2022-11-10 15:23:50 +0200

    NRP/BE: added a delete route to warehouse entity

---
+++ prisma/migrations/20221110131259_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Warehouse" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Warehouse_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "warehouseId" INTEGER NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Warehouse_name_key" ON "Warehouse"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_warehouseId_fkey" FOREIGN KEY ("warehouseId") REFERENCES "Warehouse"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/warehouse/warehouse.controller.ts
+++ src/warehouse/warehouse.controller.ts
@@ -1,23 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { WarehouseService } from './warehouse.service';
-import { Warehouse } from '@prisma/client';
 
 @Controller()
 export class WarehouseController {
   constructor(private readonly warehouseService: WarehouseService) {}
 
   @Get('warehouse/')
-  async getAllWarehouses(): Promise<Warehouse[]> {
+  async getAllWarehouses() {
     return this.warehouseService.getAllWarehouses();
   }
 
   @Get('warehouse/:id')
-  async getWareHouseAndProducts(@Param('id') id: string): Promise<Warehouse> {
+  async getWareHouseAndProducts(@Param('id') id: string) {
     return this.warehouseService.getWarehouseAndProducts(id);
   }
 
   @Post('warehouse/')
-  async createWarehouse(@Body('name') name: string): Promise<Warehouse> {
+  async createWarehouse(@Body('name') name: string) {
     return this.warehouseService.createWarehouse(name);
+  }
+
+  @Delete('warehouse/')
+  async deleteWarehouse(@Body('name') name: string) {
+    return this.warehouseService.deleteWarehouse(name);
   }
 }
